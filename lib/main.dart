640a50e87ae8a12827b6ecc63e13168139a73ebb
Ritesh <riteshhajare97@gmail.com>
2023-09-02 18:17:31 +0530

    buttons

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is title'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 213, 150, 13),
+      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                print('evivated button');
+              },
+              child: Text("Ebutton"))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('button pressed');
+        },
+        backgroundColor: Colors.deepOrangeAccent,
+        child: const Text(
+          'click',
+        ),
+      ),
+    );
+  }
+}
